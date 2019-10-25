e7f9274b28cc1d472b370a7a93f4b87f25f4d451
leiyu66 <leiyu@outlook.com>
2019-10-25 10:16:48 -0400

    Update ost-config.php

--- include/ost-config.php
+++ include/ost-config.php
@@ -39,9 +39,9 @@ define('ADMIN_EMAIL','demouser@opsgility.com');
 # ---------------------------------------------------
 # Mysql Login info
 define('DBTYPE','mysql');
-define('DBHOST','yourservername.mysql.database.azure.com');
+define('DBHOST','lyosticketsrv01.mysql.database.azure.com');
 define('DBNAME','osticket');
-define('DBUSER','demouser@yourservername');
+define('DBUSER','demouser@lyosticketsrv01');
 define('DBPASS','demo@pass123');
 
 # Table prefix
